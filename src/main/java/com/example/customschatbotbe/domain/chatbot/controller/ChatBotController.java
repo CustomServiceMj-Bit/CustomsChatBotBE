fb9d9878c62da0d757fca6234d0f8af3cbb4cfb0
dongyeon1031 <ehddus0087@naver.com>
2025-08-05 16:24:39 +0900

    revert: 세션 코드 전부 삭제

--- src/main/java/com/example/customschatbotbe/domain/chatbot/controller/ChatBotController.java
+++ src/main/java/com/example/customschatbotbe/domain/chatbot/controller/ChatBotController.java
@@ -4,7 +4,6 @@ import com.example.customschatbotbe.domain.chatbot.service.ChatBotService;
 import com.example.customschatbotbe.domain.chatbot.dto.request.ChatRequest;
 import com.example.customschatbotbe.domain.chatbot.dto.response.ChatResponse;
 import com.example.customschatbotbe.global.exception.response.ApiResponse;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +21,8 @@ public class ChatBotController {
     private final ChatBotService chatBotService;
 
     @PostMapping
-    public Mono<ResponseEntity<ApiResponse<ChatResponse>>> chat(@RequestBody ChatRequest chatRequest, HttpServletRequest httpServletRequest) {
-        String sessionId = httpServletRequest.getSession().getId();
-        System.out.println("sessionId: "+sessionId);
-        return chatBotService.generateReply(chatRequest, sessionId)
+    public Mono<ResponseEntity<ApiResponse<ChatResponse>>> chat(@RequestBody ChatRequest chatRequest) {
+        return chatBotService.generateReply(chatRequest)
                 .map(chatResponse -> ResponseEntity.ok(ApiResponse.success(SUCCESS, chatResponse)));
     }
 }
